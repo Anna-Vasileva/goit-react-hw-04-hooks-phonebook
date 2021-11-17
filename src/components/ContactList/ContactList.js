18c8cf00ee79740eb9f7d8fa48de6c203551fcf9
Anna Vasileva <anette.vasileva@gmail.com>
2021-11-17 23:06:50 +0300

    rewrites classes to hooks
    and removes comments

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -3,38 +3,7 @@ import s from "./ContactList.module.css";
 import ContactListItem from "../ContactListItem";
 import PropTypes from "prop-types";
 
-// class ContactList extends Component {
-//   render() {
-//     const { contacts, filter, onDelete } = this.props;
-//     const normalizeFilter = filter.toLowerCase();
-//     const visibleContacts = contacts.filter((el) =>
-//       el.name.toLowerCase().includes(normalizeFilter)
-//     );
-
-//     return (
-//       <ul className={s.list}>
-//         {visibleContacts.map(({ name, number, id }) => {
-//           return (
-//             <li key={id} className={s.item}>
-//               <ContactListItem
-//                 name={name}
-//                 number={number}
-//                 id={id}
-//                 onDelete={onDelete}
-//               />
-//             </li>
-//           );
-//         })}
-//       </ul>
-//     );
-//   }
-// }
-const ContactList = ({ contacts, filter, onDelete }) => {
-  // const normalizeFilter = filter.toLowerCase();
-  // const visibleContacts = contacts.filter((el) =>
-  //   el.name.toLowerCase().includes(normalizeFilter)
-  // );
-
+const ContactList = ({ contacts, onDelete }) => {
   return (
     <ul className={s.list}>
       {contacts.map(({ name, number, id }) => {
@@ -60,7 +29,6 @@ ContactList.propTypes = {
       id: PropTypes.string.isRequired,
     })
   ),
-  filter: PropTypes.string.isRequired,
   onDelete: PropTypes.func.isRequired,
 };
 
--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -2,22 +2,6 @@ import React from "react";
 import s from "./Filter.module.css";
 import PropTypes from "prop-types";
 
-// class Filter extends Component {
-//   render() {
-//     const { filter, changeFilter } = this.props;
-//     return (
-//       <label className={s.label}>
-//         Find contacts by name
-//         <input
-//           type="text"
-//           value={filter}
-//           onChange={changeFilter}
-//           className={s.input}
-//         ></input>
-//       </label>
-//     );
-//   }
-// }
 const Filter = ({ filter, changeFilter }) => {
   return (
     <label className={s.label}>
